575708360317e5dd4549ec6eb1ce1ce5d1b42e6b
Rashed <rashed700@gmail.com>
2022-02-24 00:39:32 -0500

    added new files

---
+++ portfolio-item.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="eng">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Rashed Protfolio</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
+		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link href="https://fonts.googleapis.com/css?family=Source+code+Pro:400,900|Source+Sans+Pro:300,900&display=swap" rel="stylesheet">
+
+	</head>
+	<body>
+		<header>
+			<div class="logo">
+				<img src="img/logo-portfolio.png" alt="logo">	
+			</div>
+			<!-- <button class="nav-toggle" aria-lable="toggle navigation">
+				<span class="hamburger"></span>
+			</button>
+			<nav class="nav">
+				<ul class="nav__list">
+					<li class="nav__item">
+						<a href="#home" class="nav__link">Home</a>
+					</li>
+					<li class="nav__item">
+						<a href="#services" class="nav__link">My Services</a>
+					</li>
+					<li class="nav__item">
+						<a href="#about" class="nav__link">About Me</a>
+					</li>
+					<li class="nav__item">
+						<a href="#work" class="nav__link">My Work</a>
+					</li>
+												
+				</ul>
+			</nav> -->
+		</header>
+ 
+		<!-- Introduction -->
+
+		<section class="intro" id="home">
+			<h1 class="section__title section__title--intro">
+				The title of <strong>my projrct</strong>
+			</h1>
+			<p class="section__subtitle section__subtitle--intro">A short subtitle</p>
+			<img src="img/portfolio/portfolio-05.jpg" alt="a picture of rashed" class="intro__img">
+		</section>
+
+		<div class="portfolio-item-individual">
+			<p>Ruby and PHP to name a few. A Web developer is usually concerned with the back end or the programming aspect of creating a website</p>
+			<img src="img/portfolio/portfolio-07.jpg">
+
+			<p>A Web developer is a kind of programmer who specializes in the development of applications relating to the World Wide Web or distributed network applications, which typically run protocols like HTTP from a Web server to a client browser using associated programming languages like HTML/CSS, C#,</p>
+			<p> Ruby and PHP to name a few. A Web developer is usually concerned with the back end or the programming aspect of creating a website or Web application and is not to be confused with a Web designer, who only deals with the aesthetics of a website or application, although many professionals have both skill sets.</p>
+
+		</div>
+
+
+		<!-- My Services -->
+
+		
+
+		<footer class="footer">
+			<a href="mailto:rashedfl123@gmail.com" class="footer__link">rashedfl123@gmail.com</a>
+			<ul class="social-list">
+				<li class="social-list__item">
+					<a href="https://www.facebook.com" class="social-list__link"><i class="fab fa-facebook"></i></a>
+				</li>
+				<li class="social-list__item">
+					<a href="https://www.youtube.com" class="social-list__link"><i class="fab fa-youtube"></i></a>
+				</li>
+				<li class="social-list__item">
+					<a href="https://www.facebook.com" class="social-list__link"><i class="fab fa-instagram"></i></a>
+				</li>
+				<li class="social-list__item">
+					<a href="https://www.github.com" class="social-list__link"><i class="fab fa-github"></i></a>
+				</li>
+
+			</ul>
+		</footer>
+
+
+		<script src="js/index.js" type="text/javascript"></script>
+
+	</body>
+</html>
